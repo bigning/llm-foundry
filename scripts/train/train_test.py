0e3b8f89c56da880df9d4c4b68fb195d24a3e8fb
Ning Wang <ning.wang@databricks.com>
2024-08-13 15:03:35 -0700

    a

--- scripts/train/train_test.py
+++ scripts/train/train_test.py
@@ -5,19 +5,19 @@ from composer.utils import dist, get_device
 log = logging.getLogger(__name__)
 
 def main():
-    log.debug(f'bigning debug init dist')
+    log.warning(f'bigning debug init dist')
     dist.initialize_dist(get_device(None), timeout=300)
 
     t = torch.tensor([2], device=f'cuda:{dist.get_local_rank()}')
 
     torch.distributed.all_reduce(t)
 
-    log.debug(f"bigning debug all reduce done")
+    log.warning(f"bigning debug all reduce done")
 
     if dist.get_global_rank() == 0:
         raise RuntimeError(f"bigning raise error on rank 0")
 
-    log.debug(f"bigning debug start 2nd all reduce on rank {dist.get_global_rank()}")
+    log.warning(f"bigning debug start 2nd all reduce on rank {dist.get_global_rank()}")
     torch.distributed.all_reduce(t)
 
 
